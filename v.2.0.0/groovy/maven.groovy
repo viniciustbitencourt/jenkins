7e170c17019ad38878a92b8c325888ee4d9a2d82
viniciustbitencourt <vn.biten@gmail.com>
2020-10-07 18:10:46 -0300

    commit changes properties

--- v.2.0.0/groovy/maven.groovy
+++ v.2.0.0/groovy/maven.groovy
@@ -9,11 +9,25 @@ import jenkins.model.Jenkins;
 
 println "############################ STARTING MAVEN CONFIG ############################"
 
-def extensions = Jenkins.instance.getExtensionList(Maven.DescriptorImpl.class)[0]
+//def extensions = Jenkins.instance.getExtensionList(Maven.DescriptorImpl.class)[0]
+
+def descriptor = new MavenInstallation.DescriptorImpl();
 
 List<MavenInstallation> installations = []
 
 mavenToool = ['name': 'maven3', 'url': 'file:/var/jenkins_home/downloads/apache-maven-3.5.2-bin.tar.gz', 'subdir': 'apache-maven-3.5.2']
+
+if(mavenToool.value.enabled) {
+    println(">>> Setting up tool: ${mavenToool}")
+
+    // def installer = new ZipExtractionInstaller(javaTool.value.label as String, javaTool.value.url as String, javaTool.value.subdir as String);
+    def installer = new ZipExtractionInstaller(mavenToool.value.get('label', ""),
+                                                    mavenToool.value.get('url', ""),
+                                                    mavenToool.value.get('subdir', "") )
+    def maven = new MavenInstallation(mavenToool.value.name as String, null, [new InstallSourceProperty([installer])])
+        installations.add(maven)
+}
+
 
 println(">>> Setting up tool: ${mavenToool.name} ")
 
